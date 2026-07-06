3b4e0fed345c607ce193b31a81fb22662923e6f1
Sanket-Bagde <92966750+Sanket-Bagde@users.noreply.github.com>
2026-07-07 03:37:52 +0530

    Refactor SQL data transformation queries
    Refactor SQL queries for data validation and standardization across multiple datasets, ensuring checks for unwanted spaces, NULLs, duplicates, and data consistency.

--- scripts/silver/data_transformations.sql
+++ scripts/silver/data_transformations.sql
@@ -3,7 +3,39 @@ Data Transformations:
   This SQL file contains various data transformation queries for data validation, standardization, and consistency checks across multiple datasets.
 */
 
+-----------------------------------
+-- Checking 'silver.crm_cust_info'
+-----------------------------------
+---Check for unwanted spaces
+--Expectation_ No Results
+SELECT cst_gndr
+FROM silver.crm_cust_info
+WHERE cst_gndr!= TRIM(cst_gndr)
 
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+SELECT 
+    cst_id,
+    COUNT(*) 
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    cst_key 
+FROM silver.crm_cust_info
+WHERE cst_key != TRIM(cst_key);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    cst_marital_status 
+FROM silver.crm_cust_info;
+
+---------------------------------
+-- Checking 'silver.crm_prd_info'
+---------------------------------
 --Check For NULLs or duplicates in Primary Key
 --Expectation: No Result
 SELECT 
@@ -12,13 +44,6 @@ COUNT(*)
 FROM bronze.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*)>1 or prd_id IS NULL
-
-
----Check for unwanted spaces
---Expectation_ No Results
-SELECT cst_gndr
-FROM silver.crm_cust_info
-WHERE cst_gndr!= TRIM(cst_gndr)
 
 ---Check for NULLs or Negative Numbers 
 ---Expectation: No Results
@@ -36,7 +61,9 @@ Select *
 from bronze.crm_prd_info
 Where prd_end_dt < prd_start_dt
 
-
+---------------------------------------
+--- Checking 'silver.crm_sales_details'
+---------------------------------------
 ---Check for invalid dates
 
 select 
@@ -77,7 +104,9 @@ OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
 OR sls_sales <=0 OR sls_quantity <=0 OR sls_price <=0
 ORDER BY sls_sales, sls_quantity, sls_price
 
-
+------------------------------------
+--- Checking 'silver.erp_cust_az12'
+------------------------------------
 --- To identify out of range dates
 SELECT DISTINCT
 bdate
@@ -95,7 +124,9 @@ FROM silver.erp_cust_az12
 
 select * from silver.erp_cust_az12
 
-
+---------------------------------------
+--- Checking 'silver.erp_loc_a101'
+---------------------------------------
 ----  Data Consistency and Standardization
 SELECT Distinct 
 cntry AS old_cntry,
@@ -106,8 +137,10 @@ CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
 END AS cntry
 FROM silver.erp_loc_a101
 ORDER BY cntry
-
-
+  
+-------------------------------------
+--- Checking 'silver.erp_px_cat_g1v2'
+-------------------------------------
 ----Check for unwanted spaces
 SELECT * FROM bronze.erp_px_cat_g1v2
 WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance)
